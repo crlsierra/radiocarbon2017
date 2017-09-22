f3b19a2e117982de7eb0049c838039306a5eb69b
Carlos A. Sierra <csierra@bgc-jena.mpg.de>
2017-09-22 17:52:23 +0200

    New scripts and assigment of students to groups

--- scripts.R
+++ scripts.R
@@ -15,3 +15,6 @@ ptcp=read.csv("~/Documents/Teach/Radiocarbon/2017/Letters/participantList.csv")
 
 paste0(ptcp$email, sep=";", collapse = " ")
 
+extrl=ptcp[ptcp$affil!="MPI-BGC",c(1,2,5,6,7)]
+write.csv(data.frame(extrl,Group=rep(1:4,6)), "~/Repos/radiocarbon2017/_data/labgroups.csv")
+
